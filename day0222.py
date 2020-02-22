fd723e632d9de5540b52f76761adc2c19c13b714
JasonYang2018 <773892339@qq.com>
2020-02-22 22:36:16 +0800

    new learn about python

---
+++ day0222.py
@@ -0,0 +1,61 @@
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+'''
+L.append('Jason')
+
+L1= list(range(20))
+print (L)
+print (L[::-1])
+
+print (L[-2])
+
+print (L[-2:-1])
+
+print (L1[-10:])
+print (L1[:-1])
+print (L1)
+print (L1[1:])
+print (L1[::-1])
+
+
+
+
+
+
+
+
+
+
+def trim(s):
+    if len(s) !=0 and s[0:1] == " ":
+        return trim(s[1:])
+    elif len(s) !=0 and s[-1] == " ":
+        return trim(s[:-1])
+
+    return s
+
+
+def findMinAndMax(L):
+    if len(L)==0:
+        return None
+    min=L[0]
+    max=L[0]
+    for x in L:
+        if x<min:
+            min=x
+        if x>max:
+            max=x
+    return min,max
+'''
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    if n>1:
+        num = fibonacci(n-1)+fibonacci(n-2)
+        return num
+    return None
+
+for n in range(6):
+   print (fibonacci(n))
