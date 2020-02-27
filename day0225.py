57998b1de93918c21ecf959780f3abe40bc47fa7
JasonYang2018 <773892339@qq.com>
2020-02-27 22:28:10 +0800

    new python

--- day0225.py
+++ day0225.py
@@ -18,8 +18,8 @@ def bSearch(array,target):
 #maopao paixu
 
 def maopaoSort(array):
-    for i in range(len(array)):
-        for j in range(len(array)-i-1):
+    for i in range(0,len(array)):
+        for j in range(0,len(array)-i-1):
             if array[j]>array[j+1]:
                 #ret = array[j]
                 #array[j]=array[j+1]
@@ -27,8 +27,23 @@ def maopaoSort(array):
                 array[j],array[j+1]=array[j+1],array[j]
     return array
 
-array = [9,8,10,15,7,3,20,14,2,6,7,5,90]
+array = [9,8,10,15,7,3,20,14,2,6,7,5,90,22,16]
 maopaoSort(array)
-print (array)
+print ('maopaopaixu:',array)
 
+#xuanzepaixu选择排序
+def selectionSort(alist):
+    for i in range(len(alist)):
+        min_index=i
+        for j in range(i+1,len(alist)):
+            if alist[j]<alist[min_index]:
+                min_index=j
+                alist[i],alist[min_index] = alist[min_index],alist[i]
 
+        #if min_index != i:
+            
+    return alist
+
+alist = [54,226,93,17,77,31,44,55,20]
+selectionSort(alist)
+print('xuanzepaixu:',alist)
